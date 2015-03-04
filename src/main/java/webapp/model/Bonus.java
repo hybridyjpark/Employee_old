a75c62b7c980ead859b12ed68914f84e0dd4c605
yjpark <pyj8470@naver.com>
2015-03-04 15:42:32 +0900

    webapp package add .

---
+++ src/main/java/webapp/model/Bonus.java
@@ -0,0 +1,10 @@
+package webapp.model;
+
+public class Bonus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/webapp/model/Selgrade.java
@@ -0,0 +1,10 @@
+package webapp.model;
+
+public class Selgrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
